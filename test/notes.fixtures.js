650b30cc62d583b16a319986fbfc345185f179be
Devon Reihl <devon.reihl@gmail.com>
2020-09-29 14:23:20 -0700

    changed fixtures to better names

--- test/notes.fixtures.js
+++ test/notes.fixtures.js
@@ -2,25 +2,25 @@ function makeNotesArray() {
   return [
     {
       id: 1,
-      name: 'First test post!',
+      name: 'John Doe',
       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
       modified: '2100-05-22T16:28:32.615Z'
     },
     {
       id: 2,
-      name: 'First test post!',
+      name: 'Mark Smith',
       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
       modified: '2100-05-22T16:28:32.615Z'
     },
     {
       id: 3,
-      name: 'First test post!',
+      name: 'David Tennant',
       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
       modified: '2100-05-22T16:28:32.615Z'
     },
     {
       id: 4,
-      name: 'First test post!',
+      name: 'Samantha Jones',
       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
       modified: '2100-05-22T16:28:32.615Z'
     },
@@ -31,12 +31,12 @@ function makeMaliciousNotes() {
   const maliciousNotes = {
     id: 911,
     modified: new Date().toISOString(),
-    name: 'Naughty naughty very naughty <script>alert("xss");</script>',
+    name: 'Lucifer <script>alert("xss");</script>',
     content: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
   }
   const expectedNotes = {
     ...maliciousNotes,
-    name: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
+    name: 'Lucifer &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
     content: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`,
   }
   return {
