c85db1db9ec5b3fef80beba122b9627aa0108547
Russ <18russjames@gmail.com>
2020-09-29 13:59:56 -0700

    edited router

--- src/notes/notes-router.js
+++ src/notes/notes-router.js
@@ -25,5 +25,45 @@ notesRouter
   })
   .catch(next)
 })
+.post(jsonParser, (req, res, next) => {
+  const { name, content } = req.body
+  const newNote = { name, content}
+
+  if (name == null) {
+    return res.status(400).json({
+      error: { message: `Missing name in request body`}
+    })
+  }
+
+  NoteService.insertNote(
+    req.app.get('db'),
+    newNote
+  )
+    .then(note => {
+      res
+        .status(201)
+        .location(path.posix.join(req.originalUrl, `/${note.id}`))
+        .json(serializeNote(note))
+    })
+    .catch(next)
+})
+
+notesRouter
+  .route('/:note_id')
+  .all((req, res, next) => {
+    NoteService.getById(req.app.get('db'), req.params.note_id)
+      .then(note => {
+        if (!note) {
+          return res.status(404).json({
+            error: { message: `Note doesn't exist` }
+          })
+        }
+        res.note = note
+        next()
+      })
+      .catch(next)
+  })
+
+  .get()
 
 module.exports = notesRouter
