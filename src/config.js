a1de36c1305e72e3a446d26af7dc7144d1925c3f
Devon Reihl <devon.reihl@gmail.com>
2020-09-30 08:45:22 -0700

    fixed deploy

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.envAPI_TOKEN || 'dummy-api-token',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:admin@localhost/noteful',
-  TEST_DATABASE_URL:  process.env.TEST.DATABASE_URL || 'postgresql://dunder_mifflin:admin@localhost/noteful-test'
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin:admin@localhost/noteful-test'
 }
