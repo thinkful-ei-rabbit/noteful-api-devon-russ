c133dc35dcb9767fbac85a55da9225bdd0961e4f
Devon Reihl <devon.reihl@gmail.com>
2020-09-29 18:28:04 -0700

    changes made for Heroku deployment

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.envAPI_TOKEN || 'dummy-api-token',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:admin@localhost/noteful'
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:admin@localhost/noteful',
+  TEST_DATABASE_URL:  process.env.TEST.DATABASE_URL || 'postgresql://dunder_mifflin:admin@localhost/noteful-test'
 }
